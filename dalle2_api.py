d5f67673e777c7fbf24d1e8d6ed32ecd83abd6d7
eric <zju_qzh@163.com>
2023-05-16 06:43:55 +0800

    better code

--- dalle2_api.py
+++ dalle2_api.py
@@ -1,10 +1,12 @@
 import openai
 import requests
 
-openai.api_key = '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' # E Dalle2
+openai.api_key = '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' # E Dalle2 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
 
+i = 0
+center_word = 'apple'
 response = openai.Image.create(
-  prompt="A baby's infectious laughter fills the room as she plays with a brightly colored toy.",
+  prompt="A single apple rests on a table, its glossy skin reflecting the morning sunlight",
   n=1,
   size="512x512"
 )
@@ -13,5 +15,5 @@ response = openai.Image.create(
 image_url = response['data'][0]['url']
 # download the image and save it locally
 img_data = requests.get(image_url).content
-with open('output_image.jpg', 'wb') as handler:
+with open(center_word + str(i)+ '.jpg', 'wb') as handler:
     handler.write(img_data)
--- generate_prompts.py
+++ generate_prompts.py
@@ -1,22 +1,47 @@
+# import openai
+# import os
+
+# openai.api_key = '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' # E prompts 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
+
+# # categories = ['apple', 'aquarium_fish', 'baby', 'bear']  # Add your categories here
+# categories = ['bed', 'bee', 'beetle', 'bicycle']  # Add your categories here
+
+# output = {}
+
+# for category in categories:
+#     responses = openai.Completion.create(
+#       engine="text-davinci-002",  # Use the latest available version
+#       prompt=f"Generate 10 sentences about {category}:\n. I will use those sentences to generate images. You can decide the style.",
+#       temperature=0.5,
+#       max_tokens=150,
+#       n=10  # Generate 10 completions
+#     )
+
+#     output[category] = [response['choices'][0]['text'].strip() for response in responses['choices']]
+
+# print(output)
+
 import openai
 import os
 
-openai.api_key = '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'
+openai.api_key = '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'
 
-# categories = ['apple', 'aquarium_fish', 'baby', 'bear']  # Add your categories here
-categories = ['bed', 'bee', 'beetle', 'bicycle']  # Add your categories here
+categories = ['whale', 'willow_tree', 'wolf', 'woman', 'worm']
+
+# categories = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'car', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
 
 output = {}
 
 for category in categories:
-    responses = openai.Completion.create(
-      engine="text-davinci-002",  # Use the latest available version
+    response = openai.Completion.create(
+      engine="text-davinci-002",  
       prompt=f"Generate 10 sentences about {category}:\n. I will use those sentences to generate images. You can decide the style.",
       temperature=0.5,
       max_tokens=150,
-      n=10  # Generate 10 completions
+      n=1
     )
 
-    output[category] = [response['choices'][0]['text'].strip() for response in responses]
+    output[category] = [choice['text'].strip() for choice in response['choices']]
 
 print(output)
+
